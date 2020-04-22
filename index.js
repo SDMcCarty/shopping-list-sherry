df22cb5374161a2f563c46d0f1fd250c9509546f
Sherry McCarty <sherrydmccarty@gmail.com>
2020-04-22 15:30:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//use DOM to add/remove HTMl els and styles
+//linking to external JQuery and local JS 
+//link app JS from
+//use this &event delegation
+
+//U enters items to purchase by entering txt and hitting return/clicking button
+//check/uncheck items on list by clicking 'check' button
+//permanently remove items from list
+
+//must use CDN-hosted version of jQuery
+//script els are at bottom of body el
+//
