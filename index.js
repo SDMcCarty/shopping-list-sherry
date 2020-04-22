e764fb419a420934d695c5261062b40d29fb6d3a
Sherry McCarty <sherrydmccarty@gmail.com>
2020-04-22 16:16:07 -0400

    finished project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //use DOM to add/remove HTMl els and styles
 //linking to external JQuery and local JS 
 //link app JS from
@@ -5,8 +7,43 @@
 
 //U enters items to purchase by entering txt and hitting return/clicking button
 //check/uncheck items on list by clicking 'check' button
-//permanently remove items from list
+//permanently remove items from list (.on('click') boi, maybe)
 
-//must use CDN-hosted version of jQuery
-//script els are at bottom of body el
-//
+//enter items in form '#js-shopping-list-form'
+//use key or by mouse click
+
+function addItem() {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    const itemToAdd = $(event.currentTarget).find('#shopping-list-entry').val();
+    console.log(itemToAdd);
+  });
+}
+
+//check/uncheck items on the list by clicking the 'check' button
+//this uses toggle
+// '.shopping-item-toggle'
+
+function checkUncheck() {
+  $('.shopping-item-toggle').on('click', event => {
+    const checked = $(event.currentTarget);
+    checked.toggleClass('shopping-item__checked');
+    console.log('check check');
+  });
+}
+
+//permanently remove items from list
+//there's a delete button, so use that?
+//delete items need event delagation (event-delegation-2)
+// '.shopping-item-delete'
+
+function deleteItem() {
+  $('div').on('click', '.shopping-item-delete', function(event) {
+    this.closest('li').remove();
+  });
+}
+
+
+$(addItem);
+$(checkUncheck);
+$(deleteItem);
